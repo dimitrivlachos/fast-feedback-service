fa7d4a2efd050d758dc7817dd63f97e302931e9b
Dimitri Vlachos <dimitri.j.vlachos@gmail.com>
2024-11-05 10:15:42 +0000

    Modify dispersion_extended test for conversion test branch

--- tests/dispersion_extended.sh
+++ tests/dispersion_extended.sh
@@ -10,32 +10,31 @@ mkdir -p _debug_output
 cd _debug_output
 
 # Create a JSON file with the detector parameters
-# cat << EOF > detector.json
-# {
-#     "pixel_size_x": 0.075,
-#     "pixel_size_y": 0.075,
-#     "distance": 306.765,
-#     "beam_center_x": 119.78358,
-#     "beam_center_y": 126.83430
-# }
-# EOF
+cat << EOF > detector.json
+{
+    "pixel_size_x": 0.075,
+    "pixel_size_y": 0.075,
+    "distance": 306.765,
+    "beam_center_x": 119.78358,
+    "beam_center_y": 126.83430
+}
+EOF
 
 # Open file descriptor 3 for writing to output_file.txt
 exec 3> output_file.txt
 
 # Extended dispersion test dataset
 # This is a good dataset to test the dispersion_extended algorithm. However this is 32bit data and requires truncation in order to be read
-# ../bin/spotfinder /dls/i24/data/2024/nr27313-319/gw/Test_Insulin/ins_big_15/ins_big_15_2_master.h5 \
-../bin/spotfinder /dls/i03/data/2024/cm37235-2/xraycentring/TestInsulin/ins_14/ins_14_24.nxs \
+../bin/spotfinder /dls/i24/data/2024/nr27313-319/gw/Test_Insulin/ins_big_15/ins_big_15_2_master.h5 \
   --min-spot-size 3 \
   --pipe_fd 3 \
   --dmin 4 \
+  --detector "$(cat detector.json)" \
   --algorithm "dispersion_extended" \
-  --images 1 \
-  --writeout
+  --images 1
 
 # Close file descriptor 3
 exec 3>&-
 
 # Delete the JSON file
-# rm detector.json
+rm detector.json
